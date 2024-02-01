282514c558554ce0536d34491a4d86537ce20880
Saeed <mr.ms.moradi@gmail.com>
2024-02-01 12:08:04 +0330

    Add device local data source

---
+++ app/src/main/java/com/example/dd/repository/DeviceLocalDataSource.kt
@@ -0,0 +1,18 @@
+package com.example.dd.repository
+
+import com.example.dd.model.DeviceInfoModel
+import com.example.dd.utils.DeviceUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class DeviceLocalDataSource @Inject constructor(
+    private val deviceUtils: DeviceUtils,
+) {
+
+    suspend fun collectDeviceInfo(): DeviceInfoModel {
+        return withContext(Dispatchers.IO) {
+            deviceUtils.collectDeviceInfo()
+        }
+    }
+}
